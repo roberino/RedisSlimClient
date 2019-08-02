bc69703f9a8012ede1da8612e840e40cd00d1864
Rob Eyres <rob.eyres@asos.com>
2019-08-02 17:36:33 +0100

    Docker test harness

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM mcr.microsoft.com/dotnet/core/sdk:2.2-alpine3.9
+
+COPY ./src /app/src
+COPY ./tests /app/tests
+WORKDIR /app/tests/RedisSlimClient.TestHarness
+RUN dotnet build ./
---
+++ tests/RedisSlimClient.TestHarness/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using RedisSlimClient.Configuration;
+using RedisSlimClient.Telemetry;
+
+namespace RedisSlimClient.TestHarness
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ClientConfiguration(args[0])
+            {
+                TelemetryWriter = new TextTelemetryWriter(Console.WriteLine, Severity.All)
+            };
+
+            using (var client = config.CreateClient())
+            {
+                while (true)
+                {
+                    try
+                    {
+                        var result = await client.PingAllAsync();
+
+                        foreach (var response in result)
+                        {
+                            Console.WriteLine($"PING {response.Endpoint}{response.Ok}: {response.Error}");
+                        }
+
+                        await client.SetStringAsync("x2", Guid.NewGuid().ToString());
+
+                        //await client.SetObjectAsync("x1", new MyDto()
+                        //{
+                        //    Stuff = Guid.NewGuid().ToString()
+                        //});
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+
+                    await Task.Delay(10);
+                }
+            }
+        }
+    }
+
+    public class MyDto
+    {
+        public string Stuff { get; set; }
+    }
+}
