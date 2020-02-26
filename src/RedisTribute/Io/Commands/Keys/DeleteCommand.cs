e3827cd7fea5847b7f371bfc3d735abb2cd11bf3
Roberino <rob.eyres.x@gmail.com>
2020-02-26 08:39:35 +0000

    Missing commands

---
+++ src/RedisTribute/Io/Commands/Keys/DeleteCommand.cs
@@ -0,0 +1,11 @@
+﻿using RedisTribute.Types;
+
+namespace RedisTribute.Io.Commands
+{
+    class DeleteCommand : RedisPrimativeCommand
+    {
+        public DeleteCommand(RedisKey key) : base("DEL", true, key)
+        {
+        }
+    }
+}
---
+++ src/RedisTribute/Io/Commands/Keys/ExpireCommand.cs
@@ -0,0 +1,32 @@
+﻿using RedisTribute.Types;
+using System;
+
+namespace RedisTribute.Io.Commands.Keys
+{
+    class ExpireCommand : RedisCommand<bool>
+    {
+        const string PExpireCommandText = "PEXPIRE";
+
+        readonly TimeSpan _expiry;
+
+        public ExpireCommand(RedisKey key, TimeSpan expiry) : base("EXPIRE", true, key)
+        {
+            _expiry = expiry;
+        }
+
+        protected override CommandParameters GetArgs()
+        {
+            if (_expiry.Milliseconds == 0)
+            {
+                return new object[] { CommandText, Key.Bytes, _expiry.TotalSeconds.ToString() };
+            }
+
+            return new object[] { PExpireCommandText, Key.Bytes, _expiry.TotalMilliseconds.ToString() };
+        }
+
+        protected override bool TranslateResult(IRedisObject redisObject)
+        {
+            return redisObject.ToLong() == 1;
+        }
+    }
+}
